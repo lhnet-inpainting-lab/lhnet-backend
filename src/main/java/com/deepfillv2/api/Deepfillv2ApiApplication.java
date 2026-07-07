0912ebee5d8479826bfe5c30482780f2e65d2c92
이정훈 <dksldsk@naver.com>
2026-07-07 15:29:24 +0900

    chore(backend): Spring Boot 프로젝트 초기 세팅

---
+++ src/main/java/com/deepfillv2/api/Deepfillv2ApiApplication.java
@@ -0,0 +1,13 @@
+package com.deepfillv2.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deepfillv2ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Deepfillv2ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deepfillv2/api/Deepfillv2ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepfillv2.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deepfillv2ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
